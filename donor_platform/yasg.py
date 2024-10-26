10ece981af588c620bf44018d03bc99949406b5c
Khusrav Bobodzhonov <husrav1709@gmail.com>
2024-10-26 23:06:56 +0500

    Update yasg.py

--- donor_platform/yasg.py
+++ donor_platform/yasg.py
@@ -6,9 +6,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Dastyor AI",
+      title="DonorSearch",
       default_version='v1',
-      description="This is backend side of Dastyor AI",
+      description="This is backend side of DonorSearch",
       license=openapi.License(name="BSD License"),
    ),
    public=True,
